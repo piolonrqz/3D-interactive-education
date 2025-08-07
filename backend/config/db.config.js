75b049680197e2c5fa86c2de84fc65b1d78669dc
Youssef Cabañero <youssefcabanero@gmail.com>
2025-08-07 13:48:50 +0800

    Added .env to backend

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
-  HOST: "localhost",         // or your online DB host
-  USER: "root",              // your MySQL username
-  PASSWORD: "seagle123",  // your MySQL password
-  DB: "sequelize-seagle",
-  dialect: "mysql",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT,
   pool: {
     max: 5,
     min: 0,
