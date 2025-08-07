d43cc3bd4debbaeca091667c2d71543bca2d9f4e
Youssef Cabañero <youssefcabanero@gmail.com>
2025-08-07 13:41:23 +0800

    Modify firebase to use .env

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -1,24 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBn1Mw-lXy6zJgkavRPZZABLFtHN0XadYQ",
-  authDomain: "seagle-auth.firebaseapp.com",
-  projectId: "seagle-auth",
-  storageBucket: "seagle-auth.firebasestorage.app",
-  messagingSenderId: "242820230362",
-  appId: "1:242820230362:web:4605861028d8a1d681c9e3",
-  measurementId: "G-ZXTKEGPPYR"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
